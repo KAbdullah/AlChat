f9131d946d5b2ad43576ac9b1b989ed2a50e41eb
Abdullah <khatibabdullah25@gmail.com>
2026-03-21 18:25:43 -0400

    Created the first instances of a modal

--- src/src/components/CreateRoom.jsx
+++ src/src/components/CreateRoom.jsx
@@ -1,14 +1,37 @@
+import { useState, useRef, useEffect } from "react";
 import { v6 as uuidv6 } from "uuid";
 
 function CreateRoom() {
+	const [showModal, setShowModal] = useState(false);
+
 	const handleRoomCreation = () => {
+		setShowModal(true);
 		const roomId = uuidv6();
 	};
 
 	return (
 		<div>
 			<button onClick={handleRoomCreation}>Create Room</button>
+			<CreateRoomModal showModal={showModal} setShowModal={setShowModal} />
 		</div>
+	);
+}
+
+function CreateRoomModal({ showModal, setShowModal }) {
+	const modalRef = useRef(null);
+
+	useEffect(() => {
+		if (showModal) {
+			modalRef.current?.showModal();
+		} else if (!showModal) {
+			modalRef.current?.close();
+		}
+	}, [showModal]);
+
+	return (
+		<dialog ref={modalRef}>
+			<button onClick={() => setShowModal(!showModal)}> X</button>
+		</dialog>
 	);
 }
 
